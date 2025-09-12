e5457c8ef6ca66e6e5da7fcbad5e813dd86d8115
chovin <chovin@users.noreply.github.com>
2025-09-12 14:58:42 +1000

    styles

--- src/main/java/com/csgroupfive/texter/App.java
+++ src/main/java/com/csgroupfive/texter/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("message"), 640, 480);
+        scene = new Scene(loadFXML("message"), 640, 400);
+        scene.getStylesheets().add(App.class.getResource("/com/csgroupfive/texter/css/main.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
