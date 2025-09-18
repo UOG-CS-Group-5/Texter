cedfc343504118a8d683cf0d42159d730b6e7e3f
chovin <chovin@users.noreply.github.com>
2025-09-18 21:56:34 +1000

    add some comments

--- src/main/java/com/csgroupfive/texter/EmailSender.java
+++ src/main/java/com/csgroupfive/texter/EmailSender.java
@@ -22,21 +22,26 @@ public class EmailSender implements Messagable, Named {
 
     public EmailSender(String numberPostfix, String emailToSmsDomain) {
         this.name = name + ": " + emailToSmsDomain;
+        // some email to sms services have a letter or so after the number in the email address
         this.numberPostfix = numberPostfix;
         this.emailToSmsDomain = emailToSmsDomain;
 
+        // grab configs
         Dotenv dotenv = Dotenv.load();
         String host = dotenv.get("SMTP_SERVER");
         String port = dotenv.get("SMTP_PORT");
         String user = dotenv.get("SMTP_EMAIL");
         String pass = dotenv.get("SMTP_PASS");
 
+        // config smtp
         Properties props = new Properties();
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", port);
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
 
+        // create a session that we'll reuse
+        // TODO: recreate session if it closes
         session = Session.getInstance(props, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -51,14 +56,20 @@ public class EmailSender implements Messagable, Named {
 
     public ApiResponseStatus send_message(String message, String recipient) {
         MimeMessage msg = new MimeMessage(session);
+        // it seems that with some SMTP servers, the smtp email is used instead of this from address
         String from = "no-reply@nodomain.com";
+        // build address of format: 6711231234@sms.gta.net
         String to = recipient + numberPostfix + "@" + emailToSmsDomain;
         try {
+            // set from and to
             msg.setFrom(new InternetAddress(from));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            // I don't believe subject is usually displayed for email to sms, so just put something
             msg.setSubject("Email To SMS");
+            // set the body of the email
             msg.setText(message); 
 
+            // send the email
             Transport.send(msg);
             System.out.println("sent email to " + to);
         } catch (MessagingException e) {
@@ -66,6 +77,7 @@ public class EmailSender implements Messagable, Named {
             e.printStackTrace();
             return ApiResponseStatus.FAILED;
         }
+        // return unknown if successful because email doesn't give us any feedback if the text message was sent
         return ApiResponseStatus.UNKNOWN;
     }
 }
